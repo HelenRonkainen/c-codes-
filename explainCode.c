5957fa2771234f384629bdcc0e8f4dda37a2c8ff
Helen Ronkainen <helenquelli@hotmail.com>
2019-04-07 11:46:51 +0300

    this is commit message

---
+++ explainCode.c
@@ -0,0 +1,56 @@
+/** 
+ * Task1:
+ * Comment the code where you see empthy "//" single line comments
+ * The command you need to explain is either next or underneath 
+ * the empthy single line comment.
+ * 
+ * You can write your answer to multible lines but all answers needs to be a comment
+ * 
+ * Use either Finnish or English when commenting the code
+ */
+
+
+//
+#include <stdio.h>
+
+//
+int main(int argc, char const *argv[])
+{
+    //
+    int amount = 5;
+
+    //
+    //
+    //
+    for(int i = 0; i < amount; i++)
+    {
+        printf("Hello, World! %d \n", i);
+    }
+
+    //
+    while(1){
+
+    }
+
+    return 0;
+}
+/**
+ * Task2:
+ * Dose the code run ever reach return, if no explain why that is so 
+ * 
+ * -
+ */
+
+/**
+ * Taks3:
+ * Explain the flow of the code.
+ * 
+ * -
+ */
+
+/**
+ * Task4:
+ * Explain the purpuse of %d in printf() function call.
+ * 
+ * -
+ */
