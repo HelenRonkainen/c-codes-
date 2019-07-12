96f33f3b6abbf0102dcf7c2aece059fb5b444198
Helen Ronkainen <helenquelli@hotmail.com>
2019-07-12 11:49:31 +0300

    Add files via upload

--- sketch_jul08a.ino
+++ sketch_jul08a.ino
@@ -1,59 +1,18 @@
-long int i;
 
-char condition =0;
 
-int main()
+// basic led blink exercise 
+int main (void) 
 {
-    // Init Arduino board
+  DDRB=0b00000001;
+ PORTB=0b00000001;
 
-    cli();//stop interrupts
 
-    // Timer1 initialization
-    // Assign first registers TCCR1A, -B and counter val. to 0
-    TCCR1A = 0; TCCR1B = 0; TCNT1 = 0;
-    // set compare match register for 0,5hz increments
-    OCR1A = 30637;
-    // turn on CTC mode    
-    TCCR1B |= (1 << WGM12);
-    // Set CS10 and CS12 bits for 1024 prescaler
-    TCCR1B |= (1 << CS12) | (1 << CS10);  
-    // enable timer compare interrupt
-    TIMSK1 |= (1 << OCIE1A);
+ while(1)
+       {
+     PORTB=0b00000001;
+     _delay_ms(1000); 
+     PORTB=0b00000000;
+     _delay_ms(1000);
+       }
 
-    
-    // Set the pin datadirection in register: 0 is input, 1 is output
-    DDRB |= B00000000;
-    // Setting port B's pin 5 to output.
-    DDRB |= 1<<PB5;
-
-    // Setting the led pin to high state
-    // Port pin state control: 0 is LOW, 1 is HIGH
-    //PORTB |= 1<<PB5;
-
-    sei();//allow interrupts
-
-    // Main loop
-    while(1)
-    {
-      // If true do this
-      if(condition){
-        // Set pin to HIGH
-        PORTB |= 1<<PB5;
-   delay(1000); 
-      }
-      else{
-        // Set pin to LOW
-        PORTB &= ~(1<<PB5);
- 
-      }
-    }
-
-    return 0;
-}
-
-// Interupt handler for timer1 CTC reg. interrupt vector
-ISR(TIMER1_COMPA_vect){
-  // Assign condition to be it's negation. True to False, False to True
-  condition = !condition;
-  TCNT1 = 0;  // Zero the timer
 }
